0e9e887fbd53db299fa1d751d30ac2ebb01afe52
shijiahao <shijiahao314@foxmail.com>
2024-01-02 17:56:38 +0800

    save

--- components/frame/UserStatus.tsx
+++ components/frame/UserStatus.tsx
@@ -31,6 +31,13 @@ export default function UserStatus() {
               : () => router.push('/login')
           }
         >
+          <svg
+            className="aria-hidden h-5 w-5 fill-current text-white transition group-hover:text-base-0"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 18"
+          >
+            <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
+          </svg>
           {/* 加了 Suspense 但是显示会闪烁 */}
           {/* <Suspense fallback={'加载中'}>
             <p>{isLogin ? username : '登录'}</p>
