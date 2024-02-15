6d5b9f28397a5807b3112ae7557ed65eb95aa75a
shijiahao <shijiahao314@foxmail.com>
2024-02-15 13:23:22 +0800

    modify userInfo page, need to develop backend and frontend function

--- app/userInfo/page.tsx
+++ app/userInfo/page.tsx
@@ -41,67 +41,90 @@ export default function UserInfoPage() {
             </div>
           </div> */}
         </div>
-        <div className="relative flex flex-shrink flex-grow flex-col overflow-auto overflow-x-hidden p-5">
-          <div className="mb-5 divide-y-2 divide-solid rounded-lg border-2 border-my-border dark:border-my-darkborder">
-            <div className="flex flex-row items-center justify-between border-my-border px-5 py-3 dark:border-my-darkborder">
-              <div className="flex flex-row">
-                <div className="text-base text-my-text0 dark:text-my-darktext0">用户名:</div>
-                <div className="text-base text-my-text0 dark:text-my-darktext0">asd{username}</div>
-              </div>
-              <button className="h-full w-16 rounded-lg border-2 border-my-border bg-my-button0 text-white hover:bg-my-primaryHover dark:border-my-darkborder dark:bg-my-darkButton0 dark:hover:bg-my-darkPrimaryHover">
-                修改
+        <div className="flex h-full w-full items-center justify-center">
+          <div className="relative flex max-w-xl flex-shrink flex-grow flex-col justify-center overflow-auto overflow-x-hidden px-5 pb-20 pt-2">
+            <div className="m-6 flex flex-col items-center justify-center space-y-2">
+              <img
+                className="h-24 w-24 rounded-full border-2 border-my-primary/80 p-[1px] dark:border-my-darkPrimary"
+                src="https://placehold.co/96x96.png"
+                alt="user avatar"
+              ></img>
+              <button className="text-my-primary hover:text-my-primaryHover dark:text-my-darkPrimary dark:hover:text-my-darkPrimaryHover">
+                编辑头像
               </button>
             </div>
-            <div className="flex flex-row items-center justify-between border-my-border px-5 py-3 dark:border-my-darkborder">
-              <div>
-                <div className="text-base text-my-text0 dark:text-my-darktext0">
-                  电子邮箱: {email}
-                </div>
-              </div>
+            <div className="mb-2 flex justify-end">
+              <svg
+                viewBox="0 0 1024 1024"
+                width="32"
+                height="32"
+                role="button"
+                onClick={() => {
+                  console.log('====================================');
+                  console.log('edit click');
+                  console.log('====================================');
+                }}
+              >
+                <path
+                  d="M898.8 422.5c-16.6 0-30 13.4-30 30v383.3c0 18.3-14.9 33.2-33.2 33.2H190.9c-18.3 0-33.2-14.9-33.2-33.2V191.2c0-18.3 14.9-33.2 33.2-33.2h385.2c16.6 0 30-13.4 30-30s-13.4-30-30-30H190.9c-51.4 0-93.2 41.8-93.2 93.2v644.7c0 51.4 41.8 93.2 93.2 93.2h644.7c51.4 0 93.2-41.8 93.2-93.2V452.5c0-16.5-13.5-30-30-30z"
+                  fill="#3988FF"
+                  p-id="4242"
+                />
+                <path
+                  d="M381.3 644.2c12.9 12.9 33.7 12.9 46.6 0L916 156.1c12.9-12.9 12.9-33.7 0-46.6-12.9-12.9-33.7-12.9-46.6 0L381.3 597.7c-12.8 12.8-12.8 33.7 0 46.5z"
+                  fill="#3988FF"
+                  p-id="4243"
+                />
+              </svg>
             </div>
-          </div>
-          <div className="mb-5 divide-y-2 divide-solid rounded-lg border-2 border-my-border dark:border-my-darkborder">
-            <div className="flex flex-row items-center justify-between border-my-border px-5 py-3 dark:border-my-darkborder">
-              <div>
-                <div className="text-base text-my-text0 dark:text-my-darktext0">
-                  XXX: {username}
-                </div>
-              </div>
+            <div className="mb-5 table-auto rounded-lg border-2 border-my-border dark:border-my-darkborder">
+              <table className="w-full table-auto">
+                <tbody className="divide-y-2 divide-my-border p-2 dark:divide-my-darkborder">
+                  <tr className="h-10">
+                    <td className="px-2">用户名:</td>
+                    <td className="px-2">
+                      <div className="flex flex-row items-center justify-between">
+                        <div>sjh{username}</div>
+                        <button className="rounded-lg bg-my-primary px-2 text-white hover:bg-my-primaryHover dark:bg-my-darkPrimary dark:hover:bg-my-darkPrimaryHover">
+                          修改密码
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr className="h-10">
+                    <td className="px-2">电子邮箱:</td>
+                    <td className="px-2">shijiahao314@foxmail.com{email}</td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
-            <div className="flex flex-row items-center justify-between border-my-border px-5 py-3 dark:border-my-darkborder">
-              <div>
-                <div className="text-base text-my-text0 dark:text-my-darktext0">
-                  XXX: {username}
-                </div>
-              </div>
+            <div className="flex items-center justify-end">
+              <button
+                className="rounded-lg border-2 border-my-border bg-my-danger px-4 py-2 font-semibold text-white hover:bg-my-dangerHover dark:border-my-darkborder dark:bg-my-darkDanger dark:hover:bg-my-darkDangerHover"
+                onClick={() => {
+                  Logout({}).then(([success, resp]: [boolean, LogoutResponse]) => {
+                    if (success) {
+                      setIsLogin(false);
+                      toast.success('登出成功，即将跳转到登录页', {
+                        autoClose: 2000,
+                        onClose: () => {
+                          router.push('/login');
+                        }
+                      });
+                    } else {
+                      toast.error(resp.msg, {
+                        autoClose: 2000,
+                        onClose: () => {
+                          router.push('/login');
+                        }
+                      });
+                    }
+                  });
+                }}
+              >
+                退出登录
+              </button>
             </div>
-          </div>
-          <div className="mb-5 flex items-center justify-end divide-y-2 divide-solid rounded-lg">
-            <button
-              className="rounded-lg border-2 border-my-border bg-my-danger px-4 py-2 font-semibold text-white hover:bg-my-dangerHover dark:border-my-darkborder dark:bg-my-darkDanger dark:hover:bg-my-darkDangerHover"
-              onClick={() => {
-                Logout({}).then(([success, resp]: [boolean, LogoutResponse]) => {
-                  if (success) {
-                    setIsLogin(false);
-                    toast.success('登出成功，即将跳转到登录页', {
-                      autoClose: 2000,
-                      onClose: () => {
-                        router.push('/login');
-                      }
-                    });
-                  } else {
-                    toast.error(resp.msg, {
-                      autoClose: 2000,
-                      onClose: () => {
-                        router.push('/login');
-                      }
-                    });
-                  }
-                });
-              }}
-            >
-              退出登录
-            </button>
           </div>
         </div>
       </div>
