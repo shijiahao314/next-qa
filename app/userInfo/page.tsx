599409adace77779146337817a3892834bffe211
shijiahao <shijiahao314@foxmail.com>
2024-02-17 11:17:52 +0800

    modify userInfo page

--- app/userInfo/page.tsx
+++ app/userInfo/page.tsx
@@ -6,6 +6,7 @@ import MyToastContainer from '@/components/frame/MyToastContainer';
 import { useBearStore } from '@/lib/store';
 import { useUserStore } from '@/lib/user';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 import { toast } from 'react-toastify';
 
 export default function UserInfoPage() {
@@ -17,6 +18,8 @@ export default function UserInfoPage() {
   const setNavOpen = useBearStore((state) => state.setNavOpen);
   const setIsLogin = useBearStore((state) => state.setIgLogin);
   const router = useRouter();
+
+  const [editStatus, setEditStatus] = useState<boolean>(false);
 
   const table = [
     {
@@ -63,12 +66,25 @@ export default function UserInfoPage() {
               />
             </svg>
           </button>
-          {/* <div>
+          <div>
             <div className="text-center text-xl font-bold md:text-start">用户信息</div>
             <div className="text-center text-sm text-my-text1 dark:text-my-darktext1 md:text-start">
-              用户信息
+              查看和编辑个人信息
             </div>
-          </div> */}
+          </div>
+          <button
+            className="h-12 w-12 place-content-center items-center rounded-lg border-[1px] border-my-border p-2 text-base font-semibold dark:border-my-darkborder dark:bg-my-darkbg1 md:hidden"
+            onClick={() => {
+              setNavOpen(true);
+            }}
+          >
+            <svg viewBox="0 0 1024 1024">
+              <path
+                d="M170.666667 213.333333h682.666666v85.333334H170.666667V213.333333z m0 512h682.666666v85.333334H170.666667v-85.333334z m0-256h682.666666v85.333334H170.666667v-85.333334z"
+                fill="#999"
+              />
+            </svg>
+          </button>
         </div>
         <div className="flex h-full w-full items-center justify-center">
           <div className="relative flex max-w-xl flex-shrink flex-grow flex-col justify-center overflow-auto overflow-x-hidden px-5 pb-20 pt-2">
@@ -82,29 +98,46 @@ export default function UserInfoPage() {
                 编辑头像
               </button>
             </div>
-            <div className="mb-2 flex justify-end">
-              <svg
-                viewBox="0 0 1024 1024"
-                width="32"
-                height="32"
-                role="button"
-                onClick={() => {
-                  console.log('====================================');
-                  console.log('edit click');
-                  console.log('====================================');
-                }}
-              >
-                <path
-                  d="M898.8 422.5c-16.6 0-30 13.4-30 30v383.3c0 18.3-14.9 33.2-33.2 33.2H190.9c-18.3 0-33.2-14.9-33.2-33.2V191.2c0-18.3 14.9-33.2 33.2-33.2h385.2c16.6 0 30-13.4 30-30s-13.4-30-30-30H190.9c-51.4 0-93.2 41.8-93.2 93.2v644.7c0 51.4 41.8 93.2 93.2 93.2h644.7c51.4 0 93.2-41.8 93.2-93.2V452.5c0-16.5-13.5-30-30-30z"
-                  fill="#3988FF"
-                  p-id="4242"
-                />
-                <path
-                  d="M381.3 644.2c12.9 12.9 33.7 12.9 46.6 0L916 156.1c12.9-12.9 12.9-33.7 0-46.6-12.9-12.9-33.7-12.9-46.6 0L381.3 597.7c-12.8 12.8-12.8 33.7 0 46.5z"
-                  fill="#3988FF"
-                  p-id="4243"
-                />
-              </svg>
+            <div className="mb-2 flex items-center justify-end space-x-2">
+              {editStatus ? (
+                <>
+                  <svg
+                    viewBox="0 0 1024 1024"
+                    width="32"
+                    height="32"
+                    role="button"
+                    onClick={() => {
+                      setEditStatus(false);
+                    }}
+                  >
+                    <path
+                      d="M382.4 876 7.4 501 43.1 465.4 380.9 803.2 983.6 149.7 1020.7 183.9Z"
+                      fill="#3988FF"
+                    />
+                  </svg>
+                </>
+              ) : (
+                <>
+                  <svg
+                    viewBox="0 0 1024 1024"
+                    width="32"
+                    height="32"
+                    role="button"
+                    onClick={() => {
+                      setEditStatus(true);
+                    }}
+                  >
+                    <path
+                      d="M898.8 422.5c-16.6 0-30 13.4-30 30v383.3c0 18.3-14.9 33.2-33.2 33.2H190.9c-18.3 0-33.2-14.9-33.2-33.2V191.2c0-18.3 14.9-33.2 33.2-33.2h385.2c16.6 0 30-13.4 30-30s-13.4-30-30-30H190.9c-51.4 0-93.2 41.8-93.2 93.2v644.7c0 51.4 41.8 93.2 93.2 93.2h644.7c51.4 0 93.2-41.8 93.2-93.2V452.5c0-16.5-13.5-30-30-30z"
+                      fill="#3988FF"
+                    />
+                    <path
+                      d="M381.3 644.2c12.9 12.9 33.7 12.9 46.6 0L916 156.1c12.9-12.9 12.9-33.7 0-46.6-12.9-12.9-33.7-12.9-46.6 0L381.3 597.7c-12.8 12.8-12.8 33.7 0 46.5z"
+                      fill="#3988FF"
+                    />
+                  </svg>
+                </>
+              )}
             </div>
             <div className="mb-5 table-auto rounded-lg border-2 border-my-border dark:border-my-darkborder">
               <table className="w-full table-auto">
@@ -114,7 +147,7 @@ export default function UserInfoPage() {
                       key={item.title}
                       className="divide-x-2 divide-dashed divide-my-border px-2 dark:divide-my-darkborder"
                     >
-                      <td className="px-2 text-center">{item.title}</td>
+                      <td className="px-2 text-center font-semibold">{item.title}</td>
                       <td className="px-4">{item.value}</td>
                     </tr>
                   ))}
@@ -125,24 +158,28 @@ export default function UserInfoPage() {
               <button
                 className="rounded-lg border-2 border-my-border bg-my-danger px-4 py-2 font-semibold text-white hover:bg-my-dangerHover dark:border-my-darkborder dark:bg-my-darkDanger dark:hover:bg-my-darkDangerHover"
                 onClick={() => {
-                  Logout({}).then(([success, resp]: [boolean, LogoutResponse]) => {
-                    if (success) {
-                      setIsLogin(false);
-                      toast.success('登出成功，即将跳转到登录页', {
-                        autoClose: 2000,
-                        onClose: () => {
-                          router.push('/login');
-                        }
-                      });
-                    } else {
-                      toast.error(resp.msg, {
-                        autoClose: 2000,
-                        onClose: () => {
-                          router.push('/login');
-                        }
-                      });
-                    }
-                  });
+                  if (editStatus) {
+                    setEditStatus(false);
+                  } else {
+                    Logout({}).then(([success, resp]: [boolean, LogoutResponse]) => {
+                      if (success) {
+                        setIsLogin(false);
+                        toast.success('登出成功，即将跳转到登录页', {
+                          autoClose: 2000,
+                          onClose: () => {
+                            router.push('/login');
+                          }
+                        });
+                      } else {
+                        toast.error(resp.msg, {
+                          autoClose: 2000,
+                          onClose: () => {
+                            router.push('/login');
+                          }
+                        });
+                      }
+                    });
+                  }
                 }}
               >
                 退出登录
--- components/frame/UserStatus.tsx
+++ components/frame/UserStatus.tsx
@@ -15,7 +15,7 @@ export default function UserStatus() {
           alt="user avatar"
         ></img>
         <div
-          className="flex h-10 w-full items-center justify-center rounded-lg bg-my-primary text-white hover:bg-my-primaryHover dark:bg-my-darkPrimary dark:hover:bg-my-darkPrimaryHover"
+          className="flex h-10 w-full items-center justify-center space-x-2 rounded-lg bg-my-primary text-white hover:bg-my-primaryHover dark:bg-my-darkPrimary dark:hover:bg-my-darkPrimaryHover"
           role="button"
           onClick={
             isLogin
@@ -32,6 +32,7 @@ export default function UserStatus() {
           >
             <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
           </svg>
+          <p className="text-base">用户信息</p>
           {/* 加了 Suspense 但是显示会闪烁 */}
           {/* <Suspense fallback={'加载中'}>
             <p>{isLogin ? username : '登录'}</p>
