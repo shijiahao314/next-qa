708a38b0288fd660a11e6eccb864b49946adee42
shijiahao <shijiahao314@foxmail.com>
2024-02-23 11:04:26 +0800

    fixme: next.config.js rewrite rule

--- next.config.js
+++ next.config.js
@@ -5,15 +5,18 @@ const withMDX = require('@next/mdx')();
 const nextConfig = {
   reactStrictMode: false,
   // 解决跨域问题
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/api/:path*',
-  //       destination: `http://123.56.65.40:8080/api/:path*`
-  //     }
-  //   ];
-  // },
-
+  async rewrites() {
+    return [
+      {
+        source: '/api/auth/login',
+        destination: `http://123.56.65.40:8080/api/auth/login`
+      },
+      {
+        source: '/api/auth/logout',
+        destination: `http://123.56.65.40:8080/api/auth/logout`
+      }
+    ];
+  },
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   async redirects() {
     return [
