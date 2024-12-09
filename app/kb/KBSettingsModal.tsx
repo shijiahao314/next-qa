e3848f93363f585b4e384c4673df9933eb1bda0e
shijiahao314 <shijiahao314@foxmail.com>
2024-12-09 11:38:29 +0800

    update

--- app/kb/KBSettingsModal.tsx
+++ app/kb/KBSettingsModal.tsx
@@ -122,7 +122,7 @@ export function KBSettingsModal({ modalOpen, setModalOpen }: ModalProps) {
             >
               {group.map((item) => (
                 // Row
-                <div className="flex w-full flex-row gap-10 px-4 py-2">
+                <div className="flex w-full flex-row gap-5 px-4 py-2">
                   {/* Left */}
                   <div className="flex w-2/5 flex-col">
                     <label className="text-nowrap text-base text-my-text0 dark:text-my-darktext0">
